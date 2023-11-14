ea76d92af03e565274c23f796fbbf0dbdfc0458b
yunji <dbswl330@gmail.com>
2023-11-15 01:50:25 +0900

    feat: 주문 입력받는 함수 구현
    입력받고 ,을 기준으로 파싱하여 리턴

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -7,6 +7,14 @@ const InputView = {
     );
     return Number(input);
   },
+  async readOrder() {
+    const orderString = await Console.readLineAsync(
+      "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+    );
+    const orderList = orderString.split(",");
+
+    return orderList;
+  },
 };
 
 export default InputView;
