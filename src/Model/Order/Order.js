7c8a2340ed07da61211d70e0c3167ed64002bec2
yunji <dbswl330@gmail.com>
2023-11-15 02:32:02 +0900

    feat: 입력된 주문이 유효한지 검증하는 함수 구현
    ,를 기준으로 나뉘어 들어온 입력 주문들이 -를 기준으로 메뉴이름과 개수가  잘 나뉘어 들어왔는지 검사한다.

---
+++ src/Model/Order/Order.js
@@ -0,0 +1,39 @@
+import {
+  AppetizerMenu,
+  MainMenu,
+  DessertMenu,
+  BeverageMenu,
+} from "../Menu/Menu.js";
+import CategoryOrder from "./CategoryOrder.js";
+
+export default class Orders {
+  constructor() {
+    // 의존성 주입으로 바꾸기
+
+    this.menuCategories = {
+      appetizer: new AppetizerMenu(),
+      main: new MainMenu(),
+      dessert: new DessertMenu(),
+      beverage: new BeverageMenu(),
+    };
+
+    this.orders = {
+      appetizer: {},
+      main: {},
+      dessert: {},
+      beverage: {},
+    };
+    this.orders = {};
+  }
+
+  validateinputOrders(inputOrders) {
+    inputOrders.forEach((inputOrder) => {
+      const itemDetails = inputOrder.split("-");
+      if (itemDetails.length !== 2) {
+        throw new Error(
+          "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
+        );
+      }
+    });
+  }
+}
