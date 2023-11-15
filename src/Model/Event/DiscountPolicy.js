da28ae0658bda689997ac9792a79f43a7fba809c
yunji <dbswl330@gmail.com>
2023-11-15 16:08:13 +0900

    feat: 각각의 할인 정책 클래스 구현
    calculateDiscountAmount() 함수는 할인금액을 계산하여 리턴한다.
    할인금액 계산은 날짜와 주문메뉴를 기반으로 한다.

--- src/Model/Event/DiscountPolicy.js
+++ src/Model/Event/DiscountPolicy.js
@@ -8,3 +8,34 @@ export class DiscountPolicy {
     return 0;
   }
 }
+
+export class ChristmasDdayDiscountPolicy extends DiscountPolicy {
+  calculateDiscountAmount() {
+    const christmasDday = this.dateInfo.getSpecificInfo("christmasDday");
+    return (24 - christmasDday) * 100 + 1000;
+  }
+}
+
+export class WeekdayDiscountPolicy extends DiscountPolicy {
+  calculateDiscountAmount() {
+    const dessertQuantity = this.orders
+      .getOrders()
+      .dessert.getTotalQuantityPerCategory();
+    return dessertQuantity * 2023;
+  }
+}
+
+export class WeekendDiscountPolicy extends DiscountPolicy {
+  calculateDiscountAmount() {
+    const mainQuantity = this.orders
+      .getOrders()
+      .main.getTotalQuantityPerCategory();
+    return mainQuantity * 2023;
+  }
+}
+
+export class SpecialDayDiscountPolicy extends DiscountPolicy {
+  calculateDiscountAmount() {
+    return 1000;
+  }
+}
