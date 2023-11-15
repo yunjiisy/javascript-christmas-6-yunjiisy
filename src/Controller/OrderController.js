8ed4addcddb4130599c986cdeb0c73d5cc5c2664
yunji <dbswl330@gmail.com>
2023-11-15 18:51:42 +0900

    feat: 주문 컨트롤러 구현
    입력값 들은 valid하지 않은 경우 다시 입력받는다.

---
+++ src/Controller/OrderController.js
@@ -0,0 +1,61 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "../View/InputView.js";
+import OutputView from "../View/OutputView.js";
+import Orders from "../Model/Order/Orders.js";
+import VisitDate from "../Model/Calendar/VisitDate.js";
+import {
+  AppetizerMenu,
+  BeverageMenu,
+  DessertMenu,
+  MainMenu,
+} from "../Model/Menu/Menu.js";
+
+class OrderController {
+  constructor() {
+    this.order = new Orders(
+      new AppetizerMenu(),
+      new MainMenu(),
+      new DessertMenu(),
+      new BeverageMenu()
+    );
+  }
+
+  async start() {
+    const visitDate = await this.getVisitDate();
+    const parsedOrders = await this.getOrder();
+
+    this.order.addOrders(parsedOrders);
+
+    this.showOrderInfo();
+  }
+
+  async getVisitDate() {
+    try {
+      this.visitDate = new VisitDate();
+      const inputVisitDate = await InputView.readDate();
+      return this.visitDate.getValidInputDate(inputVisitDate);
+    } catch (error) {
+      Console.print(error.message);
+      return this.getVisitDate();
+    }
+  }
+
+  async getOrder() {
+    try {
+      const orderString = await InputView.readOrder();
+      return this.order.getValidOrder(orderString);
+    } catch (error) {
+      Console.print(error.message);
+      return this.getOrder();
+    }
+  }
+
+  showOrderInfo() {
+    OutputView.printMenu(this.order.getOrderDetails());
+    OutputView.printTotalAmount(this.order.calculateTotalAmount());
+  }
+}
+
+export default OrderController;
+const startOrder = new OrderController();
+startOrder.start();
