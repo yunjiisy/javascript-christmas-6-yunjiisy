af21d3c2f23d59d20346bac91dde9b6aafa8281b
yunji <dbswl330@gmail.com>
2023-11-15 18:54:14 +0900

    refactor: App.js에서 주문 컨트롤러 호출해 프로그램 실행하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import OrderController from "./Controller/OrderController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const orderController = new OrderController();
+    await orderController.start();
+  }
 }
 
 export default App;
