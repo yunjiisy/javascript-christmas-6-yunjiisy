a800eff0490d453072c26acffa0542944913e73d
yunji <dbswl330@gmail.com>
2023-11-15 16:09:54 +0900

    feat: 할인 정책의 이름을 반환하는 함수 추가

--- src/Model/Event/DiscountPolicy.js
+++ src/Model/Event/DiscountPolicy.js
@@ -7,12 +7,20 @@ export class DiscountPolicy {
   calculateDiscountAmount() {
     return 0;
   }
+
+  getDiscountName() {
+    return "";
+  }
 }
 
 export class ChristmasDdayDiscountPolicy extends DiscountPolicy {
   calculateDiscountAmount() {
     const christmasDday = this.dateInfo.getSpecificInfo("christmasDday");
     return (24 - christmasDday) * 100 + 1000;
+  }
+
+  getDiscountName() {
+    return "크리스마스 디데이 할인";
   }
 }
 
@@ -23,6 +31,10 @@ export class WeekdayDiscountPolicy extends DiscountPolicy {
       .dessert.getTotalQuantityPerCategory();
     return dessertQuantity * 2023;
   }
+
+  getDiscountName() {
+    return "평일 할인";
+  }
 }
 
 export class WeekendDiscountPolicy extends DiscountPolicy {
@@ -32,10 +44,18 @@ export class WeekendDiscountPolicy extends DiscountPolicy {
       .main.getTotalQuantityPerCategory();
     return mainQuantity * 2023;
   }
+
+  getDiscountName() {
+    return "주말 할인";
+  }
 }
 
 export class SpecialDayDiscountPolicy extends DiscountPolicy {
   calculateDiscountAmount() {
     return 1000;
+  }
+
+  getDiscountName() {
+    return "특별 할인";
   }
 }
