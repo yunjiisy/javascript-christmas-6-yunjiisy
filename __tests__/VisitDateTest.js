cd2ddc87e572b08e837824e7b8c26e9c6e0f9179
yunji <dbswl330@gmail.com>
2023-11-15 22:07:22 +0900

    test: visitDate class test 작성

---
+++ __tests__/VisitDateTest.js
@@ -0,0 +1,22 @@
+import VisitDate from "../src/Model/Calendar/VisitDate";
+
+describe("VisitDate", () => {
+  test("유효한 날짜를 넣으면 그 유효한 날짜를 리턴한다.", () => {
+    const visitDate = new VisitDate();
+    expect(visitDate.getValidInputDate(15)).toBe(15);
+  });
+
+  test("정수(숫자)가 아닌 것을 입력하면 예외를 발생시킨다.", () => {
+    expect(() => {
+      const visitDate = new VisitDate();
+      visitDate.getValidInputDate("first");
+    }).toThrow("[ERROR]");
+  });
+
+  test("날짜 범위 밖의 숫자를 입력하면 예외를 발생시킨다.", () => {
+    expect(() => {
+      const visitDate = new VisitDate();
+      visitDate.getValidInputDate(32);
+    }).toThrow("[ERROR]");
+  });
+});
