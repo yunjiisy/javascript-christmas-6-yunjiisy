621759f544fafd3e4af6074096d08fae739f74ac
yunji <dbswl330@gmail.com>
2023-11-15 18:30:59 +0900

    feat: 이벤트를 관리하고 적용하는 클래스를 만들고 constructor를 정의

--- src/Model/Event/EventHandler.js
+++ src/Model/Event/EventHandler.js
@@ -19,53 +19,5 @@ class EventHandler {
     this.giftMenu = new GiftMenu(this.orders.calculateTotalAmount());
     this.badge = new Badge(this.calculateBenefitAmount());
   }
-
-  getBenefitList() {
-    if (this.#benefitList.length === 0) {
-      return "없음";
-    }
-
-    return this.#benefitList.join("\n");
-  }
-
-  getGiftMenu() {
-    if (this.giftMenu.getGiftMenu().length !== 0) {
-      return `${this.giftMenu.getGiftMenu()} ${
-        this.giftMenu.getGiftMenu().length
-      }개`;
-    }
-    return "없음";
-  }
-
-  getBadge() {
-    return this.badge.getBadge();
-  }
-
-  makeBenefitList() {
-    this.discount.discountPolicies.forEach((policy) => {
-      this.#benefitList.push(
-        `${policy.getDiscountName()}: -${policy.calculateDiscountAmount()}원`
-      );
-    });
-    if (this.giftMenu.getGiftMenu().length !== 0) {
-      this.#benefitList.push(
-        `증정 이벤트: -${this.giftMenu.getGiftMenuPrice()}원`
-      );
-    }
-  }
-
-  calculatePayAmount() {
-    return (
-      this.orders.calculateTotalAmount() -
-      this.discount.calculateTotalDiscountAmount()
-    );
-  }
-
-  calculateBenefitAmount() {
-    return (
-      this.discount.calculateTotalDiscountAmount() +
-      this.giftMenu.getGiftMenuPrice()
-    );
-  }
 }
 export default EventHandler;
